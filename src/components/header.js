123ceb7899cb50937a22340917f1e83f9bb0a5e3
Luis Salas <luisalas@gmail.com>
2020-01-06 16:53:41 -0500

    Cambio nombre sitio a No Name

--- src/components/header.js
+++ src/components/header.js
@@ -6,40 +6,50 @@ const Header = ({ siteTitle, activeMenu }) => (
   <header>
     <div className="container-fluid">
       <div className="header d-lg-flex justify-content-between align-items-center py-3 px-sm-3">
-        
         <div id="logo">
           <h1>
             <Link to="/">
               <span className="logo mr-2">
-                <img src="/ueofcweb/images/icon-puple-64.png" />
+                <img src="/ueofcweb/src/images/icon-puple-64.png" />
               </span>
-              UltronEle
+              No Name
             </Link>
           </h1>
         </div>
-        
+
         <div className="nav_w3ls">
           <nav>
-            <label htmlFor="drop" className="toggle">Menu</label>
+            <label htmlFor="drop" className="toggle">
+              Menu
+            </label>
             <input type="checkbox" id="drop" />
             <ul className="menu">
               <li>
-                <Link to="/" className={activeMenu=='index'?'active':''}>
+                <Link to="/" className={activeMenu == "index" ? "active" : ""}>
                   Home
                 </Link>
               </li>
               <li>
-                <Link to="/product" className={activeMenu=='product'?'active':''}>
+                <Link
+                  to="/product"
+                  className={activeMenu == "product" ? "active" : ""}
+                >
                   Product
                 </Link>
               </li>
               <li>
-                <Link to="/price" className={activeMenu=='price'?'active':''}>
+                <Link
+                  to="/price"
+                  className={activeMenu == "price" ? "active" : ""}
+                >
                   Pricing
                 </Link>
               </li>
               <li>
-                <Link to="/contact" className={activeMenu=='contact'?'active':''}>
+                <Link
+                  to="/contact"
+                  className={activeMenu == "contact" ? "active" : ""}
+                >
                   Contact Us
                 </Link>
               </li>
@@ -49,19 +59,31 @@ const Header = ({ siteTitle, activeMenu }) => (
 
         <div className="d-flex mt-lg-1 mt-sm-2 mt-3 justify-content-center">
           <div className="search-w3layouts mr-3">
-            <form action="#" method="post" className="search-bottom-wthree d-flex">
-              <input className="search" type="search" placeholder="Search Here..." required="" />
+            <form
+              action="#"
+              method="post"
+              className="search-bottom-wthree d-flex"
+            >
+              <input
+                className="search"
+                type="search"
+                placeholder="Search Here..."
+                required=""
+              />
               <button className="form-control btn" type="button">
                 <span className="fa fa-search"></span>
               </button>
             </form>
           </div>
-        
-          <a className="dwn-w3ls btn" href="https://github.com/runbytech/ultron-ele" target="_blank">
+
+          <a
+            className="dwn-w3ls btn"
+            href="https://github.com/runbytech/ultron-ele"
+            target="_blank"
+          >
             <span className="fab fa-github"></span>
           </a>
         </div>
-
       </div>
     </div>
   </header>
