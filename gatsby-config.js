ad45c4ef6f3ede37eb9dfed33f02f576062e07c6
Luis Salas <luisalas@gmail.com>
2020-01-12 16:48:49 -0500

    Update Plugin Analytics (tracking script) 2 body

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,7 @@ module.exports = {
         // The property ID; the tracking code won't be generated without it
         trackingId: process.env.GOOGLE_ANALYTICS_ID,
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
+        head: false,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
