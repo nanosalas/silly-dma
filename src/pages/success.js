34b1716391ad39412beda05e86da99edf5210fb6
Luis F. Salas <luisalas@gmail.com>
2020-01-25 15:11:27 -0500

    Create Thank You page!

---
+++ src/pages/success.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ContactPage = () => (
+  <Layout activeMenu="contact">
+    <SEO title="Contact" />
+
+    <div className="banner_w3lspvt-2">
+      <ol className="breadcrumb">
+        <li className="breadcrumb-item">
+          <Link to="/" className="font-weight-bold">
+            Home
+          </Link>
+        </li>
+        <li className="breadcrumb-item" aria-current="page">
+          Gracias
+        </li>
+      </ol>
+    </div>
+
+    <div>
+      <p>Gracias</p>
+    </div>
+  </Layout>
+)
+
+export default ContactPage
